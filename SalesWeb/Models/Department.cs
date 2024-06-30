a6aa3221f3f80d31ac39ed2faa6e35f0f83a8d0f
Elisberto Junior <elisberto_jr@hotmail.com>
2024-06-29 22:49:03 -0300

    Adicionado informacoes no banco

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,4 +1,6 @@
-﻿namespace SalesWeb.Models
+﻿using System;
+
+namespace SalesWeb.Models
 {
     public class Department
     {
@@ -6,6 +8,26 @@
 
         public string Name { get; set; }
 
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+
+        public Double TotalSales(DateTime inital, DateTime final)
+        {
+            return Sellers.Sum(sl => sl.TotalSales(inital, final));
+        }
     }
 }
--- SalesWeb/Program.cs
+++ SalesWeb/Program.cs
@@ -15,9 +15,17 @@ namespace SalesWeb
                  ));
 
             // Add services to the container.
+            builder.Services.AddScoped<SeedingService>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var seedingService = services.GetRequiredService<SeedingService>();
+                seedingService.Seed();
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -25,6 +33,7 @@ namespace SalesWeb
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                
             }
 
             app.UseHttpsRedirection();
